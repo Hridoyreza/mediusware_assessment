03762e0479b8e32c7dc890f51f5736c4d06a9674
Hridoy Reza <rezaurhridoy@gmail.com>
2022-01-21 20:59:09 +0600

    Updated

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,7 +1,30 @@
 from django.views import generic
+from django.views.generic import ListView, CreateView, UpdateView
 
 from product.models import Variant
+from product.models import Product
+from product.models import ProductVariant
 
+class ProductView(ListView):
+    template_name = 'products/list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        filter_string = {}
+        print(self.request.GET)
+        for key in self.request.GET:
+            if self.request.GET.get(key):
+                filter_string[key] = self.request.GET.get(key)
+        # return ProductVariant.objects.filter(**filter_string)
+        return ProductVariant.objects.select_related('variant', 'product').filter(**filter_string)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        context['request'] = ''
+        if self.request.GET:
+            context['request'] = self.request.GET['title__icontains']
+        return context
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
